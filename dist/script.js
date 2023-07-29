dee9c9b1156e20d416b703d63ac157c2947258d7
Kristel <kristelclaire.02@gmail.com>
2023-07-29 13:29:41 +0100

    Add mobile design
    Create responsive design for Phone and Tablet with all animations and
    effects

---
+++ dist/script.js
@@ -0,0 +1,31 @@
+const navBar = document.querySelector('nav');
+const hamburgerMenu = document.querySelector('#hamburger-menu');
+const xIcon = document.querySelector('#x-icon');
+const phoneMenu = document.querySelector('.phone-menu');
+const phoneMenuLinks = document.querySelectorAll('.phone-links');
+
+window.addEventListener('scroll', changeNavColor);
+
+hamburgerMenu.addEventListener('click', showPhoneMenu);
+xIcon.addEventListener('click', hidePhoneMenu);
+phoneMenuLinks.forEach(link => {
+    link.addEventListener('click', hidePhoneMenu);
+});
+
+function showPhoneMenu() {
+    phoneMenu.classList.remove('invisible');
+}
+
+function hidePhoneMenu() {
+    phoneMenu.classList.add('invisible');
+}
+
+function changeNavColor() {
+    if (window.scrollY > 730) {
+        navBar.classList.add('nav-scrolled'); 
+        hamburgerMenu.classList.add('hamburger-menu-scrolled');
+    } else if (window.scrollY <= 730) {
+        navBar.classList.remove('nav-scrolled');
+        hamburgerMenu.classList.remove('hamburger-menu-scrolled');
+    }
+}
