08cbecc4836e188caa4a853659072f97b1e95a18
Kristel <kristelclaire.02@gmail.com>
2023-07-30 18:14:27 +0100

    Create the navigation bar with JS only
    Disable HTML code for the navigation bar so that it can be created with
    JS only
    
    Note:   script.js is used for testing only, it all will be adjusted in
            index.js later

--- script.js
+++ script.js
@@ -1,11 +1,65 @@
-const navBar = document.querySelector('nav');
-const hamburgerMenu = document.querySelector('#hamburger-menu');
+const container = document.querySelector('#content');
+
+const NavBar = (() => {
+    const nav = document.createElement('nav');
+    const logoLink = document.createElement('a');
+    const hamburgerMenu = document.createElement('img');
+    const navLinks = document.createElement('div');
+    const ul = document.createElement('ul');
+    
+    const homeLi = document.createElement('li');
+    const aboutUsLi = document.createElement('li');
+    const menuLi = document.createElement('li');
+    const contactsLi = document.createElement('li');
+    const homeLink = document.createElement('a');
+    const aboutUsLink = document.createElement('a');
+    const menuLink = document.createElement('a');
+    const contactsLink = document.createElement('a');
+
+    logoLink.setAttribute('href', '#home');
+    logoLink.setAttribute('id', 'nav-logo');
+    logoLink.textContent = "Roberto's";
+    hamburgerMenu.setAttribute('src', 'imgs/icon-menu.png');
+    hamburgerMenu.setAttribute('alt', 'Hamburger menu');
+    hamburgerMenu.setAttribute('id', 'hamburger-menu');
+
+    navLinks.setAttribute('id', 'nav-links');
+    homeLink.setAttribute('href', '#home');
+    homeLink.classList.add('uppercase');
+    homeLink.textContent = 'Home';
+    aboutUsLink.setAttribute('href', '#about-us');
+    aboutUsLink.classList.add('uppercase');
+    aboutUsLink.textContent = 'About us';
+    menuLink.setAttribute('href', '#menu');
+    menuLink.classList.add('uppercase');
+    menuLink.textContent = 'Menu';
+    contactsLink.setAttribute('href', '#contacts');
+    contactsLink.classList.add('uppercase');
+    contactsLink.textContent = 'Contacts';
+
+    nav.append(logoLink, hamburgerMenu, navLinks);
+    navLinks.appendChild(ul);
+    ul.append(homeLi, aboutUsLi, menuLi, contactsLi);
+    homeLi.appendChild(homeLink);
+    aboutUsLi.appendChild(aboutUsLink);
+    menuLi.appendChild(menuLink);
+    contactsLi.appendChild(contactsLink);
+
+    return {
+        nav,
+        hamburgerMenu
+    }
+})();
+
+container.prepend(NavBar.nav);
+
+
 const xIcon = document.querySelector('#x-icon');
 const phoneMenu = document.querySelector('.phone-menu');
 const phoneMenuLinks = document.querySelectorAll('.phone-links');
 
 window.addEventListener('scroll', changeNavColor);
-hamburgerMenu.addEventListener('click', showPhoneMenu);
+NavBar.hamburgerMenu.addEventListener('click', showPhoneMenu);
 xIcon.addEventListener('click', hidePhoneMenu);
 
 phoneMenuLinks.forEach(link => {
@@ -22,10 +76,10 @@ function hidePhoneMenu() {
 
 function changeNavColor() {
     if (window.scrollY > 730) {
-        navBar.classList.add('nav-scrolled'); 
-        hamburgerMenu.classList.add('hamburger-menu-scrolled');
+        NavBar.nav.classList.add('nav-scrolled'); 
+        NavBar.hamburgerMenu.classList.add('hamburger-menu-scrolled');
     } else if (window.scrollY <= 730) {
-        navBar.classList.remove('nav-scrolled');
-        hamburgerMenu.classList.remove('hamburger-menu-scrolled');
+        NavBar.nav.classList.remove('nav-scrolled');
+        NavBar.hamburgerMenu.classList.remove('hamburger-menu-scrolled');
     }
 }
